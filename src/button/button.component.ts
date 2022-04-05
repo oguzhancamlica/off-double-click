7ddddefc1e722dba572d903ec346a7edc6c4671f
Oğuzhan Çamlıca <oguzhancamlica@hotmail.com>
2022-04-05 03:20:15 +0300

    fix: click event

--- src/button/button.component.ts
+++ src/button/button.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
     selector: 'loader-button',
-    template: `<button mat-button appLoader><ng-content></ng-content></button>`
+    template: `<button mat-button appLoader (click)="onClick.emit()"><ng-content></ng-content></button>`
 })
 
 export class LoaderButtonComponent implements OnInit {
+    @Output() onClick = new EventEmitter();
+
     constructor() { }
 
     ngOnInit() { }
